446f9116ed798a7bdf5e74e3af8cefafedb29b7e
Toshaksha <toshaksha02@gmail.com>
2025-07-01 10:58:48 +0530

    Polished prompts and added emojis

--- Fake_Headline_Generator/fake _headline_generator.py
+++ Fake_Headline_Generator/fake _headline_generator.py
@@ -1,14 +1,18 @@
 import random
 
+print("📰 Welcome to the Fake News Headline Generator!")
+print("Get ready for some bizarre and hilarious headlines...\n")
+
 list_of_subjects = ["Aliens", "Politicians", "Scientists", "Celebrities"]
 list_of_actions = ["declare", "discover", "blame", "ban"]
 list_of_objects = ["a new planet", "immortality", "the moon is fake", "talking dogs"]
 
 while True:
-    response = input("Want to generate a news headline (y/n): ").strip().lower()
+    response = input("Generate a fake news headline? (y/n): ").strip().lower()
     if response == "y":
-        print(f"{random.choice(list_of_subjects)} {random.choice(list_of_actions)} {random.choice(list_of_objects)}")
+        print(f"🗞️Headline: {random.choice(list_of_subjects)} {random.choice(list_of_actions)} {random.choice(list_of_objects)}")
     elif response == "n":
+        print("Thanks for reading today's fake headlines! 🐶📰")
         break
     else:
-        print("Invalid Character!! Enter 'y' or 'n'.")
+        print("⚠️ Invalid Character!! Enter 'y' or 'n'.")
