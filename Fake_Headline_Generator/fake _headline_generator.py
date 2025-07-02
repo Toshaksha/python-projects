1299066eb2f7d8d17105345880acbaf92cef8ad4
Toshaksha <toshaksha02@gmail.com>
2025-07-02 16:33:25 +0530

    Add error handling with try-except

--- Fake_Headline_Generator/fake _headline_generator.py
+++ Fake_Headline_Generator/fake _headline_generator.py
@@ -7,12 +7,15 @@ list_of_subjects = ["Aliens", "Politicians", "Scientists", "Celebrities"]
 list_of_actions = ["declare", "discover", "blame", "ban"]
 list_of_objects = ["a new planet", "immortality", "the moon is fake", "talking dogs"]
 
-while True:
-    response = input("Generate a fake news headline? (y/n): ").strip().lower()
-    if response == "y":
-        print(f"🗞️Headline: {random.choice(list_of_subjects)} {random.choice(list_of_actions)} {random.choice(list_of_objects)}")
-    elif response == "n":
-        print("Thanks for reading today's fake headlines! 🐶📰")
-        break
-    else:
-        print("⚠️ Invalid Character!! Enter 'y' or 'n'.")
+try:
+    while True:
+        response = input("Generate a fake news headline? (y/n): ").strip().lower()
+        if response == "y":
+            print(f"🗞️Headline: {random.choice(list_of_subjects)} {random.choice(list_of_actions)} {random.choice(list_of_objects)}")
+        elif response == "n":
+            print("Thanks for reading today's fake headlines! 🐶📰")
+            break
+        else:
+            print("⚠️ Invalid Character!! Enter 'y' or 'n'.")
+except Exception as e:
+    print("Oops ! Something went wrong: ", e)
