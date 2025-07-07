4451261bb7405bbdddc597254333196dcfee1e64
Toshaksha <toshaksha02@gmail.com>
2025-07-07 18:42:41 +0530

    Implement main guessing loop

--- Word_guess/word_guess.py
+++ Word_guess/word_guess.py
@@ -38,3 +38,32 @@ def levels():
 
 def no_of_letters(word):
     print("Number of letters in the word is: ", len(word))
+
+def game_logic():
+    no_of_guesses = 0
+    word = levels()
+    no_of_letters(word)
+    user_guess = "_" * len(word)
+    guess_list = list(user_guess)
+
+    while True:
+        print("Current guess:", "".join(guess_list))
+        character = input("Enter a letter (or 'q' to quit): ").strip().lower()
+
+        if character == 'q':
+            print("Game quit. The word was:", word)
+            break
+
+        no_of_guesses += 1
+        if character in word:
+            for index, item in enumerate(word):
+                if item == character:
+                    guess_list[index] = character
+        else:
+            print("Wrong guess!")
+
+        user_guess = "".join(guess_list)
+
+        if user_guess == word:
+            print("Congratulations!!! You guessed the word correctly in", no_of_guesses, "guesses.")
+            break
