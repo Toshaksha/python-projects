2a1528fb1d0528dd29f34ddd394406eec2a07d71
Toshaksha <toshaksha02@gmail.com>
2025-07-13 20:06:21 +0530

    added user input messages and score count

--- Rock_Paper_Scissor/rock_paper_scissor_game.py
+++ Rock_Paper_Scissor/rock_paper_scissor_game.py
@@ -12,24 +12,36 @@ obj_list = ["rock", "paper", "scissor"]
 comp_move = random.choice(obj_list)
 print(f"Computer choose: {comp_move}")
 
+comp_score = 0
+user_score = 0
+
 if user_move == "rock":
     if comp_move == "paper":
         print("Computer Won!")
+        comp_score += 1
     elif comp_move == "scissor":
         print("You won!")
+        user_score += 1
     else:
         print("Draw!")
 elif user_move == "paper":
     if comp_move == "scissor":
         print("Computer Won!")
+        comp_score += 1
     elif comp_move == "rock":
         print("You won!")
+        user_score += 1
     else:
         print("Draw!")
 elif user_move == "scissor":
     if comp_move == "rock":
         print("Computer Won!")
+        comp_score += 1
     elif comp_move == "paper":
         print("You won!")
+        user_score += 1
     else:
         print("Draw!")
+
+print(f"\nComputer Score is: {comp_score}")
+print(f"Your Score is: {user_score}")
