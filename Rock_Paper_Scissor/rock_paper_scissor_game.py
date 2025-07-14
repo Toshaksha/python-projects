5d12910ed5aec91c4882ec79a96b6879ee1231be
Toshaksha <toshaksha02@gmail.com>
2025-07-14 21:37:16 +0530

    converted code into functions and added score update after each round

--- Rock_Paper_Scissor/rock_paper_scissor_game.py
+++ Rock_Paper_Scissor/rock_paper_scissor_game.py
@@ -1,47 +1,78 @@
 import random
 
-print("Welcome to the Rock Paper Scissor Game!!\n")
-print("Make your move!")
-user_move = input("Rock , Paper or Scissor:- ").strip().lower()
+def welcome_message():
+    response = input("Wanna begin the game!! ").strip().lower()
+    return response in ("yes", "y", "sure")
 
-if user_move not in ["rock", "paper", "scissor"]:
-    print("Please enter correct option.")
-    quit()
+def user_selection():
+    print("\nMake your move!")
+    while True:
+        user_move = input("Rock , Paper or Scissor:- ").strip().lower()
+        if user_move not in ["rock", "paper", "scissor"]:
+            print("Invalid Selection!!")
+            continue
+        else:
+            break
+    return user_move
 
-obj_list = ["rock", "paper", "scissor"]
-comp_move = random.choice(obj_list)
-print(f"Computer choose: {comp_move}")
+def comp_selection():
+    obj_list = ["rock", "paper", "scissor"]
+    comp_choice = random.choice(obj_list)
+    print(f"Computer Choice is: {comp_choice}")
+    return comp_choice
 
-comp_score = 0
-user_score = 0
+def play_game(user_move, comp_choice):
+    comp_score = 0
+    user_score = 0
+    if user_move == "rock":
+        if comp_choice == "paper":
+            print("Computer Won!")
+            comp_score += 1
+        elif comp_choice == "scissor":
+            print("You won!")
+            user_score += 1
+        else:
+            print("Draw!")
+    elif user_move == "paper":
+        if comp_choice == "scissor":
+            print("Computer Won!")
+            comp_score += 1
+        elif comp_choice == "rock":
+            print("You won!")
+            user_score += 1
+        else:
+            print("Draw!")
+    elif user_move == "scissor":
+        if comp_choice == "rock":
+            print("Computer Won!")
+            comp_score += 1
+        elif comp_choice == "paper":
+            print("You won!")
+            user_score += 1
+        else:
+            print("Draw!")
+    return user_score, comp_score
 
-if user_move == "rock":
-    if comp_move == "paper":
-        print("Computer Won!")
-        comp_score += 1
-    elif comp_move == "scissor":
-        print("You won!")
-        user_score += 1
-    else:
-        print("Draw!")
-elif user_move == "paper":
-    if comp_move == "scissor":
-        print("Computer Won!")
-        comp_score += 1
-    elif comp_move == "rock":
-        print("You won!")
-        user_score += 1
-    else:
-        print("Draw!")
-elif user_move == "scissor":
-    if comp_move == "rock":
-        print("Computer Won!")
-        comp_score += 1
-    elif comp_move == "paper":
-        print("You won!")
-        user_score += 1
-    else:
-        print("Draw!")
+def score_output(comp_score, user_score):
+    print(f"\nComputer Score is: {comp_score}")
+    print(f"Your Score is: {user_score}\n")
 
-print(f"\nComputer Score is: {comp_score}")
-print(f"Your Score is: {user_score}")
+def main():
+    total_user_score = 0
+    total_comp_score = 0
+    while True:
+        if not welcome_message():
+            print("Thanks for Playing!!!!")
+            quit()
+
+        user_move = user_selection()
+        comp_move = comp_selection()
+
+        user_score, comp_score = play_game(user_move, comp_move)
+
+        total_user_score += user_score
+        total_comp_score += comp_score
+
+        score_output(total_comp_score, total_user_score)
+
+main()
