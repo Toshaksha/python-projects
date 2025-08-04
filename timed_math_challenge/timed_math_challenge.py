a4ce2ecf2b8fc7cd8b706527e7a50916eafd6030
Toshaksha <toshaksha02@gmail.com>
2025-08-04 17:36:30 +0530

    Add user-friendly prompts

--- timed_math_challenge/timed_math_challenge.py
+++ timed_math_challenge/timed_math_challenge.py
@@ -1,37 +1,42 @@
 import random
 import time
 
-OPERATORS = ["+", "-", "*"]
+OPERATORS = ["+" , "-" ,"*" ]
 MIN_OPERAND = 2
 MAX_OPERAND = 12
 TOTAL_PROBLEMS = 10
 
 def generate_problem():
-    left = random.randint(MIN_OPERAND, MAX_OPERAND)
+    left = random.randint(MIN_OPERAND,MAX_OPERAND)
     operator = random.choice(OPERATORS)
     right = random.randint(MIN_OPERAND, MAX_OPERAND)
 
-    expression = str(left) + " " + operator + " " + str(right)
-    ans = eval(expression)
-    return expression, ans
+    expr = str(left) + " " + operator + " " +str(right)
+    answ = eval(expression)
+    return expr, answ
 
-input("Press enter to start.")
+
+input("Press enter to start the timed math challenge.")
 print("------------------------------------")
 start_time = time.time()
 attempts = 0
 
 for i in range(TOTAL_PROBLEMS):
-    expression, ans = generate_problem()
+    expression , ans = generate_problem()
     while True:
-        guess = input(f"Problem #{i+1}: {expression} = ")
+        guess = input(f"Problem #{str(i+1)}: {expression}= ")
         try:
-            attempts += 1
+            attempts +=1
             if int(guess) == ans:
+                print("Correct!\n")
                 break
+            else:
+                print("Incorrect, try again.")
         except ValueError:
             print("Enter a valid answer.")
 
 end_time = time.time()
 total_time = end_time - start_time
 
+print("------------------------------------")
 print(f"You finished the quiz in {total_time:.2f} seconds with {attempts} attempts.")
