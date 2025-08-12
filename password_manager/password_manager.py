5694019f0d164bc55c398c43821d52e9cd54fe9b
Toshaksha <toshaksha02@gmail.com>
2025-08-12 20:19:23 +0530

    Add functions to organize code

--- password_manager/password_manager.py
+++ password_manager/password_manager.py
@@ -1,21 +1,30 @@
 PASSWORD = "password.txt"
 
+
+def add():
+    a_name = input("Account Name: ")
+    pwd = input("Password: ")
+    with open(PASSWORD, "a") as file:
+        file.write(a_name + "|" + pwd + "\n")
+    print("Password saved successfully!")
+
+
+def view():
+    with open(PASSWORD, "r") as file:
+        lines = file.readlines()
+        for line in lines:
+            account, password = line.strip().split("|")
+            print(f"Account: {account}, Password: {password}")
+
+
 while True:
     mode = input("Type 'add' to save, 'view' to read, or 'q' to quit: ").strip().lower()
 
     if mode == "q":
         break
     elif mode == "add":
-        a_name = input("Account Name: ")
-        pwd = input("Password: ")
-        with open(PASSWORD, "a") as file:
-            file.write(a_name + "|" + pwd + "\n")
-        print("Password saved successfully!")
+        add()
     elif mode == "view":
-        with open(PASSWORD, "r") as file:
-            lines = file.readlines()
-            for line in lines:
-                account, password = line.strip().split("|")
-                print(f"Account: {account}, Password: {password}")
+        view()
     else:
         print("Invalid mode.")
