afcbf9912ae9786d9f3092c692914033ea47dff3
Toshaksha <toshaksha02@gmail.com>
2025-08-19 20:05:40 +0530

    Basic mad lib generator using user inputs and f-string

---
+++ madlib_generator/madlib_generator.py
@@ -0,0 +1,8 @@
+print("Today I went to the [place] and saw a [adjective] [animal]. It was [verb] loudly!")
+place = input("place: ")
+adjective = input("adjective: ")
+animal = input("animal: ")
+verb = input("verb: ")
+
+story_1 = f"Today I went to the {place} and saw a {adjective} {animal}. It was {verb} loudly!"
+print(story_1)
