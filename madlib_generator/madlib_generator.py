b077df0fb271f6b11fda2678400bf3533ee6ce83
Toshaksha <toshaksha02@gmail.com>
2025-08-20 21:12:57 +0530

    Added multiple story functions and random selection feature

--- madlib_generator/madlib_generator.py
+++ madlib_generator/madlib_generator.py
@@ -1,8 +1,21 @@
-print("Today I went to the [place] and saw a [adjective] [animal]. It was [verb] loudly!")
-place = input("place: ")
-adjective = input("adjective: ")
-animal = input("animal: ")
-verb = input("verb: ")
+import random
 
-story_1 = f"Today I went to the {place} and saw a {adjective} {animal}. It was {verb} loudly!"
-print(story_1)
+def story_1():
+    place = input("Enter a place: ")
+    adjective = input("Enter an adjective: ")
+    animal = input("Enter an animal: ")
+    verb = input("Enter a verb ending with -ing: ")
+    emotion = input("Enter an emotion: ")
+    past_verb = input("Enter a past tense verb: ")
+    print(f"One day, I went to the {place} and saw a {adjective} {animal} {verb} at me. I was so {emotion}, I {past_verb}!")
+
+def story_2():
+    food = input("Enter a type of food: ")
+    noun = input("Enter a noun: ")
+    verb = input("Enter a verb ending with -ing: ")
+    family_member = input("Enter a family member: ")
+    emotion = input("Enter an emotion: ")
+    print(f"Yesterday, I tried to make {food}, added too much {noun}, and it started {verb}. My {family_member} was so {emotion}!")
+
+stories = [story_1, story_2]
+random.choice(stories)()
